df005f268334e6cd22c3abb49eca9ac6fe19ae0b
Sveinung Gundersen <sveinugu@gmail.com>
2016-06-16 16:41:16 +0200

    Changed doc of getInputBoxNames

--- lib/proto/tools/ToolTemplate.py
+++ lib/proto/tools/ToolTemplate.py
@@ -31,40 +31,40 @@ class ToolTemplate(GeneralGuiTool):
         Note: the key has to be camelCase and start with a non-capital letter
               (e.g. "firstKey")
 
-        Mandatory method for all ProTo tools.
+        Optional method. Default return value if method is not defined: []
         """
         return [('First header', 'firstKey'),
                 ('Second Header', 'secondKey')]
 
-    @classmethod
-    def getInputBoxOrder(cls):
-        """
-        Specifies the order in which the input boxes should be displayed,
-        as a list. The input boxes are specified by index (starting with 1)
-        or by key. If None, the order of the input boxes is in the order
-        specified by getInputBoxNames().
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
-
-    @classmethod
-    def getInputBoxGroups(cls, choices=None):
-        """
-        Creates a visual separation of groups of consecutive option boxes
-        from the rest (fieldset). Each such group has an associated label
-        (string), which is shown to the user. To define groups of option
-        boxes, return a list of BoxGroup namedtuples with the label, the key
-        (or index) of the first and last options boxes (inclusive).
-
-        Example:
-           from quick.webtool.GeneralGuiTool import BoxGroup
-           return [BoxGroup(label='A group of choices', first='firstKey',
-                            last='secondKey')]
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
+    # @classmethod
+    # def getInputBoxOrder(cls):
+    #     """
+    #     Specifies the order in which the input boxes should be displayed,
+    #     as a list. The input boxes are specified by index (starting with 1)
+    #     or by key. If None, the order of the input boxes is in the order
+    #     specified by getInputBoxNames().
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def getInputBoxGroups(cls, choices=None):
+    #     """
+    #     Creates a visual separation of groups of consecutive option boxes
+    #     from the rest (fieldset). Each such group has an associated label
+    #     (string), which is shown to the user. To define groups of option
+    #     boxes, return a list of BoxGroup namedtuples with the label, the key
+    #     (or index) of the first and last options boxes (inclusive).
+    #
+    #     Example:
+    #        from quick.webtool.GeneralGuiTool import BoxGroup
+    #        return [BoxGroup(label='A group of choices', first='firstKey',
+    #                         last='secondKey')]
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
 
     @classmethod
     def getOptionsBoxFirstKey(cls):  # Alt: getOptionsBox1()
@@ -171,48 +171,48 @@ class ToolTemplate(GeneralGuiTool):
         """
         return ''
 
-    @classmethod
-    def getInfoForOptionsBoxKey(cls, prevChoices):
-        """
-        If not None, defines the string content of an clickable info box
-        beside the corresponding input box. HTML is allowed.
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
-
-    @classmethod
-    def getDemoSelections(cls):
-        """
-        Defines a set of demo inputs to the option boxes in the
-        order defined by getOptionBoxNames and getOptionsBoxOrder.
-        If not None, a Demo button appears in the interface. Clicking the
-        button fills the option boxed with the defined demo values.
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return ['testChoice1', '..']
-
-    @classmethod
-    def getExtraHistElements(cls, choices):
-        """
-        Defines extra history elements to be created when clicking execute.
-        This is defined by a list of HistElement objects, as in the
-        following example:
-
-           from proto.GeneralGuiTool import HistElement
-           return [HistElement(cls.HISTORY_TITLE, 'bed', hidden=False)]
-
-        It is good practice to use class constants for longer strings.
-
-        In the execute() method, one typically needs to fetch the path to
-        the dataset referred to by the extra history element. To fetch the
-        path, use the dict cls.extraGalaxyFn with the defined history title
-        as key, e.g. "cls.extraGalaxyFn[cls.HISTORY_TITLE]".
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
+    # @classmethod
+    # def getInfoForOptionsBoxKey(cls, prevChoices):
+    #     """
+    #     If not None, defines the string content of an clickable info box
+    #     beside the corresponding input box. HTML is allowed.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def getDemoSelections(cls):
+    #     """
+    #     Defines a set of demo inputs to the option boxes in the
+    #     order defined by getOptionBoxNames and getOptionsBoxOrder.
+    #     If not None, a Demo button appears in the interface. Clicking the
+    #     button fills the option boxed with the defined demo values.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return ['testChoice1', '..']
+    #
+    # @classmethod
+    # def getExtraHistElements(cls, choices):
+    #     """
+    #     Defines extra history elements to be created when clicking execute.
+    #     This is defined by a list of HistElement objects, as in the
+    #     following example:
+    #
+    #        from proto.GeneralGuiTool import HistElement
+    #        return [HistElement(cls.HISTORY_TITLE, 'bed', hidden=False)]
+    #
+    #     It is good practice to use class constants for longer strings.
+    #
+    #     In the execute() method, one typically needs to fetch the path to
+    #     the dataset referred to by the extra history element. To fetch the
+    #     path, use the dict cls.extraGalaxyFn with the defined history title
+    #     as key, e.g. "cls.extraGalaxyFn[cls.HISTORY_TITLE]".
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
 
     @classmethod
     def execute(cls, choices, galaxyFn=None, username=''):
@@ -243,150 +243,150 @@ class ToolTemplate(GeneralGuiTool):
         """
         return None
 
-    @classmethod
-    def getSubToolClasses(cls):
-        """
-        Specifies a list of classes for subtools of the main tool. These
-        subtools will be selectable from a selection box at the top of the
-        page. The input boxes will change according to which subtool is
-        selected.
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
-
-    @classmethod
-    def isPublic(cls):
-        """
-        Specifies whether the tool is accessible to all users. If False, the
-        tool is only accessible to a restricted set of users as well as admin
-        users, as defined in the galaxy.ini file.
-
-        Optional method. Default return value if method is not defined: False
-        """
-        return False
-
-    @classmethod
-    def isRedirectTool(cls):
-        """
-        Specifies whether the tool should redirect to an URL when the Execute
-        button is clicked.
-
-        Optional method. Default return value if method is not defined: False
-        """
-        return False
-
-    @classmethod
-    def getRedirectURL(cls, choices):
-        """
-        This method is called to return an URL if the isRedirectTool method
-        returns True.
-
-        Mandatory method if isRedirectTool() returns True.
-        """
-        return ''
-
-    @classmethod
-    def isHistoryTool(cls):
-        """
-        Specifies if a History item should be created when the Execute button
-        is clicked.
-
-        Optional method. Default return value if method is not defined: True
-        """
-        return True
-
-    @classmethod
-    def isBatchTool(cls):
-        """
-        Specifies if this tool could be run from batch using the batch. The
-        batch run line can be fetched from the info box at the bottom of the
-        tool.
-
-        Deprecated method, will be removed.
-        """
-        return cls.isHistoryTool()
-
-    @classmethod
-    def isDynamic(cls):
-        """
-        Specifies whether changing the content of textboxes causes the page
-        to reload. Returning False stops the need for reloading the tool
-        after each input, resulting in less lags for the user.
-
-        Optional method. Default return value if method is not defined: True
-        """
-        return True
-
-    @classmethod
-    def getResetBoxes(cls):
-        """
-        Specifies a list of input boxes which resets the subsequent stored
-        choices previously made. The input boxes are specified by index
-        (starting with 1) or by key.
-
-        Optional method. Default return value if method is not defined: True
-        """
-        return []
-
-    @classmethod
-    def getToolDescription(cls):
-        """
-        Specifies a help text in HTML that is displayed below the tool.
-
-        Optional method. Default return value if method is not defined: ''
-        """
-        return ''
-
-    @classmethod
-    def getToolIllustration(cls):
-        """
-        Specifies an id used by StaticFile.py to reference an illustration
-        file on disk. The id is a list of optional directory names followed
-        by a filename. The base directory is STATIC_PATH as defined by
-        Config.py. The full path is created from the base directory
-        followed by the id.
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
-
-    @classmethod
-    def getFullExampleURL(cls):
-        """
-        Specifies an URL to an example page that describes the tool, for
-        instance a Galaxy page.
-
-        Optional method. Default return value if method is not defined: None
-        """
-        return None
-
-    @classmethod
-    def isDebugMode(cls):
-        """
-        Specifies whether the debug mode is turned on. Debug mode is
-        currently mostly used within the Genomic HyperBrowser and will make
-        little difference in a plain Galaxy ProTo installation.
-
-        Optional method. Default return value if method is not defined: False
-        """
-        return False
-
-    @classmethod
-    def getOutputFormat(cls, choices):
-        """
-        The format of the history element with the output of the tool. Note
-        that if 'html' is returned, any print statements in the execute()
-        method is printed to the output dataset. For text-based output
-        (e.g. bed) the output dataset only contains text written to the
-        galaxyFn file, while all print statements are redirected to the info
-        field of the history item box.
-
-        Note that for 'html' output, standard HTML header and footer code is
-        added to the output dataset. If one wants to write the complete HTML
-        page, use the restricted output format 'customhtml' instead.
-
-        Optional method. Default return value if method is not defined:
-        'html'
-        """
-        return 'html'
+    # @classmethod
+    # def getSubToolClasses(cls):
+    #     """
+    #     Specifies a list of classes for subtools of the main tool. These
+    #     subtools will be selectable from a selection box at the top of the
+    #     page. The input boxes will change according to which subtool is
+    #     selected.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def isPublic(cls):
+    #     """
+    #     Specifies whether the tool is accessible to all users. If False, the
+    #     tool is only accessible to a restricted set of users as well as admin
+    #     users, as defined in the galaxy.ini file.
+    #
+    #     Optional method. Default return value if method is not defined: False
+    #     """
+    #     return False
+    #
+    # @classmethod
+    # def isRedirectTool(cls):
+    #     """
+    #     Specifies whether the tool should redirect to an URL when the Execute
+    #     button is clicked.
+    #
+    #     Optional method. Default return value if method is not defined: False
+    #     """
+    #     return False
+    #
+    # @classmethod
+    # def getRedirectURL(cls, choices):
+    #     """
+    #     This method is called to return an URL if the isRedirectTool method
+    #     returns True.
+    #
+    #     Mandatory method if isRedirectTool() returns True.
+    #     """
+    #     return ''
+    #
+    # @classmethod
+    # def isHistoryTool(cls):
+    #     """
+    #     Specifies if a History item should be created when the Execute button
+    #     is clicked.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return True
+    #
+    # @classmethod
+    # def isBatchTool(cls):
+    #     """
+    #     Specifies if this tool could be run from batch using the batch. The
+    #     batch run line can be fetched from the info box at the bottom of the
+    #     tool.
+    #
+    #     Deprecated method, will be removed.
+    #     """
+    #     return cls.isHistoryTool()
+    #
+    # @classmethod
+    # def isDynamic(cls):
+    #     """
+    #     Specifies whether changing the content of textboxes causes the page
+    #     to reload. Returning False stops the need for reloading the tool
+    #     after each input, resulting in less lags for the user.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return True
+    #
+    # @classmethod
+    # def getResetBoxes(cls):
+    #     """
+    #     Specifies a list of input boxes which resets the subsequent stored
+    #     choices previously made. The input boxes are specified by index
+    #     (starting with 1) or by key.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return []
+    #
+    # @classmethod
+    # def getToolDescription(cls):
+    #     """
+    #     Specifies a help text in HTML that is displayed below the tool.
+    #
+    #     Optional method. Default return value if method is not defined: ''
+    #     """
+    #     return ''
+    #
+    # @classmethod
+    # def getToolIllustration(cls):
+    #     """
+    #     Specifies an id used by StaticFile.py to reference an illustration
+    #     file on disk. The id is a list of optional directory names followed
+    #     by a filename. The base directory is STATIC_PATH as defined by
+    #     Config.py. The full path is created from the base directory
+    #     followed by the id.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def getFullExampleURL(cls):
+    #     """
+    #     Specifies an URL to an example page that describes the tool, for
+    #     instance a Galaxy page.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def isDebugMode(cls):
+    #     """
+    #     Specifies whether the debug mode is turned on. Debug mode is
+    #     currently mostly used within the Genomic HyperBrowser and will make
+    #     little difference in a plain Galaxy ProTo installation.
+    #
+    #     Optional method. Default return value if method is not defined: False
+    #     """
+    #     return False
+    #
+    # @classmethod
+    # def getOutputFormat(cls, choices):
+    #     """
+    #     The format of the history element with the output of the tool. Note
+    #     that if 'html' is returned, any print statements in the execute()
+    #     method is printed to the output dataset. For text-based output
+    #     (e.g. bed) the output dataset only contains text written to the
+    #     galaxyFn file, while all print statements are redirected to the info
+    #     field of the history item box.
+    #
+    #     Note that for 'html' output, standard HTML header and footer code is
+    #     added to the output dataset. If one wants to write the complete HTML
+    #     page, use the restricted output format 'customhtml' instead.
+    #
+    #     Optional method. Default return value if method is not defined:
+    #     'html'
+    #     """
+    #     return 'html'
