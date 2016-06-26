358bdcd634deac1ff8647d43199496e2073364ba
Abdulrahman Azab <eng.azab@gmail.com>
2016-06-26 02:23:03 +0100

    Update generictool.py

--- lib/proto/generictool.py
+++ lib/proto/generictool.py
@@ -24,9 +24,9 @@ from collections import namedtuple, OrderedDict
 from urllib import quote, unquote
 #from gold.application.GalaxyInterface import GalaxyInterface
 #from quick.webtools.GeneralGuiToolsFactory import GeneralGuiToolsFactory
-#from quick.webtools.GeneralGuiTool import HistElement
 #from quick.util.StaticFile import StaticImage
-#from gold.result.HtmlCore import HtmlCore
+from proto.tools.GeneralGuiTool import HistElement
+from proto.HtmlCore import HtmlCore
 from proto.config.Config import URL_PREFIX, GALAXY_BASE_DIR
 #from gold.application.LogSetup import usageAndErrorLogging
 #from gold.util.CommonFunctions import getClassName
