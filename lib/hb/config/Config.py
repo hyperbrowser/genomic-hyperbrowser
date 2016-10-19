b498e5ddb2181e18ca7e4a0e404807cb7c14328e
FlorianK <flo.krull@gmail.com>
2016-10-19 15:12:05 +0200

    fix import path

--- lib/hb/config/Config.py
+++ lib/hb/config/Config.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with The Genomic HyperBrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-from config.AutoConfig import *
+from AutoConfig import *
 
 #
 # Version information
