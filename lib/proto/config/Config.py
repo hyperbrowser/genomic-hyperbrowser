82aabe52a6f1a04b5e33bb71aef4dcd346726824
Sveinung Gundersen <sveinugu@gmail.com>
2016-11-24 11:35:40 +0100

    ProTo: Moved config parsing to GalaxyConfigParser. Made sure dynamic reading of galaxy.ini is supported

--- lib/proto/config/Config.py
+++ lib/proto/config/Config.py
@@ -1,28 +1,29 @@
-from proto.config.Security import GALAXY_SECURITY_HELPER_OBJ, GALAXY_BASE_DIR, getFromConfig, getUniverseConfigParser
+from proto.config.GalaxyConfigParser import GALAXY_BASE_DIR, GalaxyConfigParser
 
 
 def getUrlPrefix(config):
-    prefix = getFromConfig(config, 'prefix', '', 'filter:proxy-prefix')
-    filterWith = getFromConfig(config, 'filter-with', '', 'app:main')
+    prefix = config.getWithDefault('prefix', '', 'filter:proxy-prefix')
+    filterWith = config.getWithDefault('filter-with', '', 'app:main')
     return prefix if filterWith == 'proxy-prefix' else ''
 
 
-config = getUniverseConfigParser()
+config = GalaxyConfigParser()
 
 if not globals().get('URL_PREFIX'):
     URL_PREFIX = getUrlPrefix(config)
 
-GALAXY_REL_TOOL_CONFIG_FILE = getFromConfig(config, 'tool_config_file', 'config/tool_conf.xml')
+GALAXY_REL_TOOL_CONFIG_FILE = config.getWithDefault('tool_config_file', 'config/tool_conf.xml')
 ADMIN_USERS = [username.strip() for username in
-               getFromConfig(config, 'admin_users', '').split(',')]
+               config.getWithDefault('admin_users', '').split(',')]
 RESTRICTED_USERS = [username.strip() for username in
-                    getFromConfig(config, 'restricted_users', '', 'galaxy_proto').split(',')]
-OUTPUT_PRECISION = int(getFromConfig(config, 'output_precision', '4', 'galaxy_proto'))
+                    config.getWithDefault('restricted_users', '', 'galaxy_proto').split(',')]
+OUTPUT_PRECISION = int(config.getWithDefault('output_precision', '4', 'galaxy_proto'))
 STATIC_REL_PATH = URL_PREFIX + '/static/proto'
 STATIC_PATH = GALAXY_BASE_DIR + '/' + STATIC_REL_PATH
 GALAXY_URL = URL_PREFIX
-GALAXY_FILE_PATH = GALAXY_BASE_DIR + '/' + getFromConfig(config, 'file_path', 'database/files')
+GALAXY_FILE_PATH = GALAXY_BASE_DIR + '/' + config.getWithDefault('file_path', 'database/files')
 
 
 def userHasFullAccess(galaxyUserName):
-    return galaxyUserName in ADMIN_USERS + RESTRICTED_USERS if galaxyUserName not in [None, ''] else False
+    return galaxyUserName in ADMIN_USERS + RESTRICTED_USERS \
+        if galaxyUserName not in [None, ''] else False
---
+++ lib/proto/config/GalaxyConfigParser.py
@@ -0,0 +1,31 @@
+# NB: imported by Galaxy (managers.hdas). Should not import other ProTo modules.
+# This module will only be loaded once, during startup of Galaxy, and not dynamically by ProTo
+# tools. This means changes in the module will not take effect until Galaxy is restarted.
+
+import os
+from ConfigParser import SafeConfigParser
+
+
+GALAXY_BASE_DIR = os.path.abspath(os.path.dirname(__file__) + '/../../../.')
+
+
+class GalaxyConfigParser(SafeConfigParser):
+    def __init__(self):
+        SafeConfigParser.__init__(self, {'here': GALAXY_BASE_DIR})
+
+        configRelFn = os.environ.get('GALAXY_CONFIG_FILE')
+
+        if not configRelFn:
+            for configRelFn in ['config/galaxy.ini', 'config/galaxy.ini.sample']:
+                configFn = GALAXY_BASE_DIR + '/' + configRelFn
+                if os.path.exists(configFn):
+                    self.read(configFn)
+                    break
+            else:
+                raise Exception('No Galaxy config file found at path: ' + configFn)
+
+    def getWithDefault(self, key, default, section='app:main'):
+        try:
+            return self.get(section, key)
+        except:
+            return default
