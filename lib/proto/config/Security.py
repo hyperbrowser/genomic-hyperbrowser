ba4231dff50d56904638a0817b1f888988ed6341
Sveinung Gundersen <sveinugu@gmail.com>
2016-11-24 11:35:40 +0100

    ProTo: Moved config parsing to GalaxyConfigParser. Made sure dynamic reading of galaxy.ini is supported
    (cherry picked from commit 82aabe52a6f1a04b5e33bb71aef4dcd346726824)

--- lib/proto/config/Security.py
+++ lib/proto/config/Security.py
@@ -1,42 +1,17 @@
 # NB: imported by Galaxy (managers.hdas). Should not import other ProTo modules.
-# This module will only be loaded once, during startup of Galaxy, and not dynamically by ProTo tools.
-# This means changes in the module will not take effect until Galaxy is restarted.
+# This module will only be loaded once, during startup of Galaxy, and not dynamically by ProTo
+# tools. This means changes in the module will not take effect until Galaxy is restarted.
 
-import os
-from ConfigParser import SafeConfigParser
-
-GALAXY_BASE_DIR = os.path.abspath(os.path.dirname(__file__) + '/../../../.')
-
-
-def getUniverseConfigParser():
-    config = SafeConfigParser({'here': GALAXY_BASE_DIR})
-    configRelFn = os.environ.get('GALAXY_CONFIG_FILE')
-    if not configRelFn:
-        configRelFn = 'config/galaxy.ini'
-    configFn = GALAXY_BASE_DIR + '/' + configRelFn
-    if os.path.exists(configFn):
-        config.read(configFn)
-    else:
-        raise Exception('No Galaxy config file found at path: ' + configFn)
-    return config
-
-
-config = getUniverseConfigParser()
-
-
-def getFromConfig(config, key, default, section='app:main'):
-    try:
-        return config.get(section, key)
-    except:
-        return default
+from proto.config.GalaxyConfigParser import GalaxyConfigParser
 
 
 def galaxyGetSecurityHelper(config):
     from galaxy.web.security import SecurityHelper
 
-    id_secret = getFromConfig(config, 'proto_id_secret',
-                              'USING THE DEFAULT IS ALSO NOT SECURE!',
-                              section='galaxy_proto')
+    config = GalaxyConfigParser()
+    id_secret = config.getWithDefault('proto_id_secret',
+                                      'USING THE DEFAULT IS ALSO NOT SECURE!',
+                                      section='galaxy_proto')
     return SecurityHelper(id_secret=id_secret)
 
 
