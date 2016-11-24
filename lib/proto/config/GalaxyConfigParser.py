dc5a57af79c17ab8e7fbc1c5d83d1a4f220febdb
Sveinung Gundersen <sveinugu@gmail.com>
2016-11-24 12:07:04 +0100

    ProTo: Fixed bug in GalaxyConfigParser in startup.

--- lib/proto/config/GalaxyConfigParser.py
+++ lib/proto/config/GalaxyConfigParser.py
@@ -13,16 +13,16 @@ class GalaxyConfigParser(SafeConfigParser):
     def __init__(self):
         SafeConfigParser.__init__(self, {'here': GALAXY_BASE_DIR})
 
-        configRelFn = os.environ.get('GALAXY_CONFIG_FILE')
-
-        if not configRelFn:
-            for configRelFn in ['config/galaxy.ini', 'config/galaxy.ini.sample']:
+        for configRelFn in [os.environ.get('GALAXY_CONFIG_FILE'),
+                            'config/galaxy.ini',
+                            'config/galaxy.ini.sample']:
+            if configRelFn:
                 configFn = GALAXY_BASE_DIR + '/' + configRelFn
                 if os.path.exists(configFn):
                     self.read(configFn)
                     break
-            else:
-                raise Exception('No Galaxy config file found at path: ' + configFn)
+        else:
+            raise Exception('No Galaxy config file found at path: ' + configFn)
 
     def getWithDefault(self, key, default, section='app:main'):
         try:
