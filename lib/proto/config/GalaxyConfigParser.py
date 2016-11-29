e231c703e6d17a663a2dd394dbd2c1a43aa14d1b
Sveinung Gundersen <sveinugu@gmail.com>
2016-11-29 13:23:35 +0100

    ProTo: Updated config parsing with type handling
    (cherry picked from commit ce1642a49a3ab23bd070fada47b0cea7e1e6fde3)

--- lib/proto/config/GalaxyConfigParser.py
+++ lib/proto/config/GalaxyConfigParser.py
@@ -2,6 +2,7 @@
 # This module will only be loaded once, during startup of Galaxy, and not dynamically by ProTo
 # tools. This means changes in the module will not take effect until Galaxy is restarted.
 
+import ast
 import os
 from ConfigParser import SafeConfigParser
 
@@ -13,19 +14,33 @@ class GalaxyConfigParser(SafeConfigParser):
     def __init__(self):
         SafeConfigParser.__init__(self, {'here': GALAXY_BASE_DIR})
 
+        self._configFn = None
+
         for configRelFn in [os.environ.get('GALAXY_CONFIG_FILE'),
                             'config/galaxy.ini',
                             'config/galaxy.ini.sample']:
             if configRelFn:
-                configFn = GALAXY_BASE_DIR + '/' + configRelFn
-                if os.path.exists(configFn):
-                    self.read(configFn)
+                self._configFn = GALAXY_BASE_DIR + '/' + configRelFn
+                if os.path.exists(self._configFn):
+                    self.read(self._configFn)
                     break
         else:
-            raise Exception('No Galaxy config file found at path: ' + configFn)
+            raise Exception('No Galaxy config file found at path: ' + self._configFn)
 
     def getWithDefault(self, key, default, section='app:main'):
         try:
-            return self.get(section, key)
+            val = self.get(section, key)
+            if not isinstance(default, basestring):
+                val = ast.literal_eval(val)
         except:
-            return default
+            val = default
+
+        if type(val) != type(default):
+            raise ValueError('Value for configuration "%s" in section "%s" of file "%s" ' %
+                             (key, section, self._configFn) +
+                             'is incorrect. The value "%s" is of different type ' % val +
+                             'than the default value "%s": %s != %s' %
+                             (default, type(val), type(default)))
+
+        return val
+
