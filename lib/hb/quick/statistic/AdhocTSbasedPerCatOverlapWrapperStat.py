d63d95ed230e7088377ecdae9a5110bbcab6796f
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2017-02-03 12:12:18 +0100

    Initial TS commit, only initial GUI in place

---
+++ lib/hb/quick/statistic/AdhocTSbasedPerCatOverlapWrapperStat.py
@@ -0,0 +1,25 @@
+class AdhocTSbasedPerCatOverlapWrapperStat(MagicStatFactory):
+    '''
+    classdocs
+    '''
+    pass
+
+
+
+class AdhocTSbasedPerCatOverlapWrapperStatUnsplittable(MultipleTrackStatistic):
+    def _compute(self):
+        res = self._children[0].getResult()
+        return res
+
+    def _createChildren(self)
+        choices_queryTrack = self._kwArgs['choices_queryTrack']
+        choices_gsuite = self._kwArgs['choices_gsuite']
+        selected_category = self._kwArgs['selected_category']
+        import GuiBasedTsFactory
+        queryTS = GuiBasedTsFactory.getSingleTrackTS(choices_queryTrack)
+        refTS = GuiBasedTsFactory.getTrackListTS(choices_gsuite)
+
+        #trackStructure = TrackStructure()
+        #self._tracks
+
+        self._addChild(AdhocTSbasedPerCatOverlapV2Stat(self._region, trackStructure, **self._kwArgs)
