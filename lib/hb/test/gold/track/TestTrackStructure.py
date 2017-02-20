289b0391444ca660fd4645284f98e9fb62f34236
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2017-02-20 23:34:36 +0100

    Make an empty file for testing TS class, so that others team members know where to put tests..

---
+++ lib/hb/test/gold/track/TestTrackStructure.py
@@ -0,0 +1,7 @@
+#Unit test for aspects/methods in TrackStructureV2
+#To be filled in by various team members - according to different tasks in Trello list:
+#  "Finalize first statistic using new TS (branch:first_new_ts_stat)"
+#See one of the other tests for how to make a unit test - e.g. TestTrackFormat.py
+#I (GKS) don't know which git process will be the easiest here -
+# I expect the easiest might be to just develop in the branch first_new_ts_stat and commit often,
+# but another possibility may be to make separate feature branches and merge in again..
