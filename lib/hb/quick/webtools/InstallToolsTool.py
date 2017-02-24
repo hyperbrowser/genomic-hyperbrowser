bd0d2dc88e8b5148980eb8e8573ce0a2ab294e48
Sveinung Gundersen <sveinugu@gmail.com>
2017-02-24 16:44:18 +0100

    Updated HB subclass of InstallToolsTool. Does not allow renaming/moving of XML file.

--- lib/hb/quick/webtools/InstallToolsTool.py
+++ lib/hb/quick/webtools/InstallToolsTool.py
@@ -1,7 +1,13 @@
+from config.Config import PROTO_HB_TOOL_DIR
 from proto.tools.InstallToolsTool import InstallToolsTool as ProtoInstallToolsTool
+from quick.webtools.GeneralGuiTool import GeneralGuiToolMixin
 
 
-class InstallToolsTool(ProtoInstallToolsTool):
+class InstallToolsTool(GeneralGuiToolMixin, ProtoInstallToolsTool):
+    TOOL_DIR = PROTO_HB_TOOL_DIR
+    TOOL_ID_PREFIX = 'hb'
+    XML_TOOL_DIR = 'proto/hyperbrowser'
+
     @classmethod
-    def getOptionsBoxToolType(cls, prevchoices):
-        return ['hb', 'proto']
+    def getOptionsBoxToolXMLPath(cls, prevChoices):
+        return cls._getToolXmlPath(prevChoices), 1, True
