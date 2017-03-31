4c301fdf2006184234b192bb80f9bc01a3f5b145
dianadom <dianadom@ifi.uio.no>
2017-03-31 16:00:07 +0200

    changes for Concatenate two or more hGsuites with extra source field

--- lib/hb/quick/webtools/hgsuite/ContacenateTwoOrMoreHGsuitesTool.py
+++ lib/hb/quick/webtools/hgsuite/ContacenateTwoOrMoreHGsuitesTool.py
@@ -1,4 +1,8 @@
+import urllib2
+
 from gold.gsuite.GSuiteEditor import addColumnToGSuite
+from gold.gsuite.GSuiteTrack import GalaxyGSuiteTrack
+from quick.multitrack.MultiTrackCommon import getGSuiteFromGSuiteFile
 from quick.webtools.GeneralGuiTool import GeneralGuiTool
 from quick.webtools.gsuite.ConcatenateGSuitesTool import ConcatenateGSuitesTool
 
@@ -6,7 +10,7 @@ from quick.webtools.gsuite.ConcatenateGSuitesTool import ConcatenateGSuitesTool
 class ContacenateTwoOrMoreHGsuitesTool(ConcatenateGSuitesTool):
     @classmethod
     def getToolName(cls):
-        return "Concatenate two or more hGsuites"
+        return "Concatenate two or more hGsuites with extra source field"
 
 
     @classmethod
@@ -15,11 +19,13 @@ class ContacenateTwoOrMoreHGsuitesTool(ConcatenateGSuitesTool):
         from gold.gsuite.GSuiteEditor import concatenateGSuites
         from gold.gsuite.GSuiteComposer import composeToFile
 
-
-        gSuiteList = [getGSuiteFromGalaxyTN(galaxyTn) for galaxyTn in \
-                      cls._getSelectedGsuiteGalaxyTNsInOrder(choices)]
-        for currentGSuite in gSuiteList:
-            addColumnToGSuite(currentGSuite, attrName='source', trackTitleToColumnValueDict='NOT DONE YET')
+        gSuiteList = []
+        for galaxyTn in cls._getSelectedGsuiteGalaxyTNsInOrder(choices):
+            currentGSuite = getGSuiteFromGalaxyTN(galaxyTn)
+            gSuiteTitle = urllib2.unquote(galaxyTn.split(':')[-1])
+            uri = GalaxyGSuiteTrack.generateURI(galaxyFn=galaxyFn)
+            outGsuite = addColumnToGSuite(currentGSuite, uri, attrName='source', gSuiteTitle=gSuiteTitle)
+            gSuiteList.append(outGsuite)
 
         concatenatedGSuite = concatenateGSuites(gSuiteList)
         composeToFile(concatenatedGSuite, galaxyFn)
