ecb2dfc5d26e62dd26d37e144136d8a46d7c64e6
Boris Simovski <boris.simovski@gmail.com>
2017-08-23 15:08:33 +0200

    Fixed bug for no overlap datasets

--- lib/hb/quick/util/NumpyUtils.py
+++ lib/hb/quick/util/NumpyUtils.py
@@ -3,10 +3,21 @@ import numpy as np
 
 
 def getNumpyMatrixMaxElement(npMat):
-    maxRowInd, maxColInd = np.unravel_index(np.nanargmax(npMat), npMat.shape)
+    maxRowInd=0
+    maxColInd=0
+    try:
+        maxRowInd, maxColInd = np.unravel_index(np.nanargmax(npMat), npMat.shape)
+    except ValueError as e:
+        pass
     return npMat[maxRowInd, maxColInd], maxRowInd, maxColInd
 
 
+
 def getNumpyMatrixMinElement(npMat):
-    minRowInd, minColInd = np.unravel_index(np.nanargmin(npMat), npMat.shape)
+    minRowInd = 0
+    minColInd = 0
+    try:
+        minRowInd, minColInd = np.unravel_index(np.nanargmin(npMat), npMat.shape)
+    except ValueError as e:
+        pass
     return npMat[minRowInd, minColInd], minRowInd, minColInd
