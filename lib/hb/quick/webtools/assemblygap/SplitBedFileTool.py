1d0c3877c4eb193cba20a23756114d1b714a0f73
dianadom <dianadom@ifi.uio.no>
2017-11-02 20:58:33 +0100

    Categorical GSuite

---
+++ lib/hb/quick/webtools/assemblygap/SplitBedFileTool.py
@@ -0,0 +1,133 @@
+from quick.application.ExternalTrackManager import ExternalTrackManager
+from quick.webtools.GeneralGuiTool import GeneralGuiTool
+
+
+class SplitBedFileTool(GeneralGuiTool):
+    @classmethod
+    def getToolName(cls):
+        return "Split bed file"
+
+    @classmethod
+    def getInputBoxNames(cls):
+        return [('Select file', 'selFile'),
+                ('Split file by', 'splittedPart'),
+                ('How much (%)', 'splittedValue')]
+
+    @classmethod
+    def getOptionsBoxSelFile(cls):
+        return GeneralGuiTool.getHistorySelectionElement('bed')
+
+    @classmethod
+    def getOptionsBoxSplittedPart(cls, prevChoices):
+        return ['start', 'middle', 'end']
+
+    @classmethod
+    def getOptionsBoxSplittedValue(cls, prevChoices):
+        return ''
+
+    @classmethod
+    def execute(cls, choices, galaxyFn=None, username=''):
+        selFile = choices.selFile
+        splittedPart = choices.splittedPart
+        splittedValue = choices.splittedValue
+        if splittedPart == 'middle':
+            splittedValue = 50 - float(splittedValue)
+
+        percentage = float(splittedValue)/100
+
+        data = []
+        with open(ExternalTrackManager.extractFnFromGalaxyTN(selFile.split(':')),'r') as f:
+            for line in f:
+                l = line.strip('\n').split('\t')
+                lineMod = l
+                try:
+                    if splittedPart == 'start':
+                        val = int(percentage*(int(l[2]) - int(l[1])) + int(l[1]))
+                        if val == int(l[1]):
+                            val = val + 1
+                        lineMod = [l[0], l[1], val]
+                    if splittedPart == 'end':
+                        val = int(int(l[2])-percentage*(int(l[2]) - int(l[1])))
+                        l[2] = int(l[2])
+                        if val == l[2] and val != 0:
+                            val = l[2] - 1
+                        if val == l[2] and val == 0:
+                            val = 1
+                            l[2] = 2
+                        lineMod = [l[0], val, l[2]]
+                    if splittedPart == 'middle':
+                        val1 = int(percentage * (int(l[2]) - int(l[1])) + int(l[1]))
+                        val2 = int(int(l[2]) - percentage * (int(l[2]) - int(l[1])))
+
+                        if val1 == val2:
+                            val2 = val2 + 1
+
+                        lineMod = [l[0], val1, val2]
+                except:
+                    pass
+                data.append(lineMod)
+
+        outputFile = open(galaxyFn, 'w')
+        for d in data:
+            outputFile.write('\t'.join(str(e) for e in d) + '\n')
+        outputFile.close()
+
+    @classmethod
+    def validateAndReturnErrors(cls, choices):
+        return None
+
+    # @classmethod
+    # def getSubToolClasses(cls):
+    #     return None
+    #
+    @classmethod
+    def isPublic(cls):
+        return True
+    #
+    # @classmethod
+    # def isRedirectTool(cls):
+    #     return False
+    #
+    # @classmethod
+    # def getRedirectURL(cls, choices):
+    #     return ''
+    #
+    # @classmethod
+    # def isHistoryTool(cls):
+    #     return True
+    #
+    # @classmethod
+    # def isBatchTool(cls):
+    #     return cls.isHistoryTool()
+    #
+    # @classmethod
+    # def isDynamic(cls):
+    #     return True
+    #
+    # @classmethod
+    # def getResetBoxes(cls):
+    #     return []
+    #
+    # @classmethod
+    # def getToolDescription(cls):
+    #     return ''
+    #
+    # @classmethod
+    # def getToolIllustration(cls):
+    #     return None
+    #
+    # @classmethod
+    # def getFullExampleURL(cls):
+    #     return None
+    #
+    # @classmethod
+    # def isDebugMode(cls):
+    #     return False
+    #
+    @classmethod
+    def getOutputFormat(cls, choices):
+        return 'bed'
+    #
+    # @classmethod
+    # def getOutputName(cls, choices=None):
+    #     return cls.getToolSelectionName()
