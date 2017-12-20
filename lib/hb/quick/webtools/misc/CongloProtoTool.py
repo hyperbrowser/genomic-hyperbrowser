25d13c79047db4e8beaa5781f80f2a03a765e4b8
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2017-12-20 16:53:13 +0100

    Debugging

--- lib/hb/quick/webtools/misc/CongloProtoTool.py
+++ lib/hb/quick/webtools/misc/CongloProtoTool.py
@@ -24,7 +24,7 @@ from quick.multitrack.MultiTrackCommon import getGSuiteFromGalaxyTN
 
 ALL_METHOD_CLASSES = [GenometriCorr, StereoGene, Giggle, IntervalStats] #LOLA, HBCongloMethod
 #[GenometriCorr, LOLA, StereoGene, Giggle, IntervalStats, HBCongloMethod]
-#debug
+#debug1
 from quick.webtools.GeneralGuiTool import GeneralGuiTool
 
 class CongloProtoTool(GeneralGuiTool):
