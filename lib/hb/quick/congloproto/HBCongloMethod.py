0018cf050670055c976784a7b626272e0479d608
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2017-12-31 09:56:26 +0100

    Minor changes

--- lib/hb/quick/congloproto/HBCongloMethod.py
+++ lib/hb/quick/congloproto/HBCongloMethod.py
@@ -96,7 +96,7 @@ class HyperBrowser(ManyVsManyMethod):
         pvals = OrderedDict()
         for trackTuple, result in self._results.iteritems():
             pval = result.getGlobalResult()['P-value']
-            pvals[trackTuple] = pval
+            pvals[trackTuple] = '%.2f' % pval
         return self.getRemappedResultDict(pvals)
 
 
