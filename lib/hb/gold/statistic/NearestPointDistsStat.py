ea54ed741a9d155b2077b922782b2617a04e5842
Geir Kjetil Sandve <geirksa@ifi.uio.no>
2018-01-30 21:06:21 +0100

    Changed to ~50x faster impl of nearest point dist

--- lib/hb/gold/statistic/NearestPointDistsStat.py
+++ lib/hb/gold/statistic/NearestPointDistsStat.py
@@ -3,7 +3,7 @@ from gold.statistic.Statistic import Statistic, StatisticConcatResSplittable, On
 from gold.statistic.RawDataStat import RawDataStat
 from gold.track.TrackFormat import TrackFormatReq
 from gold.util.CustomExceptions import NotSupportedError
-from gold.track.TrackView import AutonomousTrackElement
+
 
 class NearestPointDistsStat(MagicStatFactory):
     pass
@@ -22,61 +22,36 @@ class NearestPointDistsStatUnsplittable(Statistic):
     def _compute(self):
         if self._region.strand == False and self._distDirection != 'both':
             raise NotSupportedError() #support by switching between left/right-computation..
-        
+
         tv1 = self._children[0].getResult()
         tv2 = self._children[1].getResult()
-        dists = []
-        tv2Iter = tv2.__iter__()
-
-        try:
-            p2 = tv2Iter.next()
-            p2Pos = p2.start()
-            prevP2 = None
-            
-        except:
-            #no points in track2            
-            return [None for el in tv1]
-        
-        emptyP2 = False
-        
-        for p1 in tv1:
-            #p1 = el1
-            try:
-                while p2Pos < p1.start():
-                    if not emptyP2:
-                        prevP2 = AutonomousTrackElement(trackEl=p2)
-                    p2 = tv2Iter.next()
-                    p2Pos = p2.start()
-                self._appendToDists(dists, p1, p2, prevP2)
-            except StopIteration:
-                emptyP2 = True
-                self._appendToDists(dists, p1, None, prevP2)
-        return dists
+        return self._determineClosestDistPerQuery(tv1.startsAsNumpyArray().tolist(), tv2.startsAsNumpyArray().tolist(), self._distDirection)
 
     @staticmethod
-    def _getObservator(p1, p2):
-        return abs(p2.start() - p1.start()) if p2 != None else None
-        
-    def _appendToDists(self, dists, p1, rightP2, leftP2):        
-        if self._distDirection == 'right':
-            dists.append( self._getObservator(p1, rightP2) )
-        elif self._distDirection == 'left':
-            if rightP2!=None and rightP2.start()==p1.start():
-                dists.append( self._getObservator(p1, rightP2) )
+    def _determineClosestDistPerQuery(ql, rl, distDirection):
+        dist = [-1] * len(ql)
+        if len(rl) == 0:
+            return [None] * len(ql)
+        ri = 0
+        for qi in range(len(ql)):
+            q = ql[qi]
+            while ri<len(rl) and rl[ri]<q:
+                ri+=1
+            leftDist = q - rl[ri-1] if ri>0 else None
+            rightDist = rl[ri]-q if ri<len(rl) else None
+            assert leftDist is not None or rightDist is not None
+            if distDirection == 'left':
+                dist[qi] = 0 if rightDist==0 else leftDist
+            elif distDirection == 'right':
+                dist[qi] = 0 if leftDist == 0 else rightDist
             else:
-                dists.append( self._getObservator(p1, leftP2) )
-        else:
-            if leftP2==rightP2==None:
-                dists.append(None)
-            else:
-                left = p1.start() - leftP2.start() if leftP2 != None else None
-                right = rightP2.start() - p1.start() if rightP2 != None else None
-                if left==None or (right!=None and left>right):
-                    dists.append( self._getObservator(p1, rightP2) )
+                if rightDist is None or (leftDist is not None and leftDist < rightDist):
+                    dist[qi] = leftDist
                 else:
-                    dists.append( self._getObservator(p1, leftP2) )
-                #dists.append( min(x for x in [left,right] if x!=None) )
-            
+                    dist[qi] = rightDist
+
+        return dist
+
     def _createChildren(self):
         self._addChild( RawDataStat(self._region, self._track, TrackFormatReq(dense=False, interval=False)) )
         self._addChild( RawDataStat(self._region, self._track2, TrackFormatReq(dense=False, interval=False)) )
