e92da3c9af0fc48b2b4428d6e23aa6f89765e4cb
dianadom <dianadom@ifi.uio.no>
2018-02-14 18:41:37 +0100

    hierarchical gSuite tool for mutation project

---
+++ lib/hb/quick/webtools/hgsuite/DivideGSuiteAccordingToColumnInTheTrackTool.py
@@ -0,0 +1,245 @@
+from collections import OrderedDict
+
+from gold.gsuite import GSuiteComposer
+from gold.gsuite.GSuite import GSuite
+from gold.gsuite.GSuiteTrack import GalaxyGSuiteTrack, GSuiteTrack
+from proto.CommonFunctions import ensurePathExists
+from quick.multitrack.MultiTrackCommon import getGSuiteFromGalaxyTN
+from quick.origdata.UcscHandler import UcscHandler
+from quick.webtools.GeneralGuiTool import GeneralGuiTool
+
+
+class DivideGSuiteAccordingToColumnInTheTrackTool(GeneralGuiTool):
+    @classmethod
+    def getToolName(cls):
+        return "Divide tracks in gSuite according to the phrase in the title column and create hierarchical gSuite"
+
+    @classmethod
+    def getInputBoxNames(cls):
+        return [('Select gSuite', 'gSuite'),
+                ('Select phrases (use colon to provide more than one phrase)', 'param')
+                ]
+
+    @classmethod
+    def getOptionsBoxGSuite(cls):
+        return GeneralGuiTool.getHistorySelectionElement('gsuite')
+
+    @classmethod
+    def getOptionsBoxParam(cls, prevChoices):
+        return ''
+
+    @classmethod
+    def execute(cls, choices, galaxyFn=None, username=''):
+
+        par = choices.param.replace(' ','').split(',')
+        gSuite = getGSuiteFromGalaxyTN(choices.gSuite)
+        attrMut = OrderedDict()
+
+
+        for a in gSuite.attributes:
+            attrMut[a] = gSuite.getAttributeValueList(a)
+
+        outputGSuite = GSuite()
+
+        for i, iTrack in enumerate(gSuite.allTracks()):
+
+            trackTitle = iTrack.title
+            trackPath = iTrack.path
+
+            for p in par:
+
+                attr = OrderedDict()
+                attr['mutation'] = str(p)
+
+                for k in attrMut.keys():
+                    attr[k] = attrMut[k][i]
+
+                uri = GalaxyGSuiteTrack.generateURI(galaxyFn=galaxyFn,
+                                                    extraFileName=str(trackTitle) + '--' + str(p), suffix='bed')
+                gSuiteTrack = GSuiteTrack(uri)
+                outFn = gSuiteTrack.path
+                ensurePathExists(outFn)
+
+                lineAll = []
+                with open(trackPath, 'r') as f:
+                    for l in f.readlines():
+                        line = l.split('\t')
+                        if p in line[3]:
+                            lineAll.append(l)
+
+                with open(outFn, 'w') as contentFile:
+                    contentFile.write(''.join(lineAll))
+                contentFile.close()
+
+                gs = GSuiteTrack(uri, title=''.join(
+                    str(trackTitle) + '--' + str(p)), genome=gSuite.genome,
+                                 attributes=attr)
+
+                outputGSuite.addTrack(gs)
+
+        GSuiteComposer.composeToFile(outputGSuite, galaxyFn)
+
+    @classmethod
+    def selectColumns(cls):
+        return ''
+
+    @classmethod
+    def validateAndReturnErrors(cls, choices):
+
+        if not choices.gSuite:
+            return 'Select gSuite'
+
+        if not choices.param:
+            return 'Select phrases'
+
+        if choices.gSuite:
+            gSuite = getGSuiteFromGalaxyTN(choices.gSuite)
+            for i, iTrack in enumerate(gSuite.allTracks()):
+                if iTrack.suffix != 'bed':
+                    return 'Your tracks need to be in .bed format'
+
+        return None
+
+    # @classmethod
+    # def getSubToolClasses(cls):
+    #     """
+    #     Specifies a list of classes for subtools of the main tool. These
+    #     subtools will be selectable from a selection box at the top of the
+    #     page. The input boxes will change according to which subtool is
+    #     selected.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    @classmethod
+    def isPublic(cls):
+        """
+        Specifies whether the tool is accessible to all users. If False, the
+        tool is only accessible to a restricted set of users as well as admin
+        users, as defined in the galaxy.ini file.
+
+        Optional method. Default return value if method is not defined: False
+        """
+        return True
+    #
+    # @classmethod
+    # def isRedirectTool(cls):
+    #     """
+    #     Specifies whether the tool should redirect to an URL when the Execute
+    #     button is clicked.
+    #
+    #     Optional method. Default return value if method is not defined: False
+    #     """
+    #     return False
+    #
+    # @classmethod
+    # def getRedirectURL(cls, choices):
+    #     """
+    #     This method is called to return an URL if the isRedirectTool method
+    #     returns True.
+    #
+    #     Mandatory method if isRedirectTool() returns True.
+    #     """
+    #     return ''
+    #
+    # @classmethod
+    # def isHistoryTool(cls):
+    #     """
+    #     Specifies if a History item should be created when the Execute button
+    #     is clicked.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return True
+    #
+    # @classmethod
+    # def isBatchTool(cls):
+    #     """
+    #     Specifies if this tool could be run from batch using the batch. The
+    #     batch run line can be fetched from the info box at the bottom of the
+    #     tool.
+    #
+    #     Optional method. Default return value if method is not defined:
+    #         same as isHistoryTool()
+    #     """
+    #     return cls.isHistoryTool()
+    #
+    # @classmethod
+    # def isDynamic(cls):
+    #     """
+    #     Specifies whether changing the content of textboxes causes the page
+    #     to reload. Returning False stops the need for reloading the tool
+    #     after each input, resulting in less lags for the user.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return True
+    #
+    # @classmethod
+    # def getResetBoxes(cls):
+    #     """
+    #     Specifies a list of input boxes which resets the subsequent stored
+    #     choices previously made. The input boxes are specified by index
+    #     (starting with 1) or by key.
+    #
+    #     Optional method. Default return value if method is not defined: True
+    #     """
+    #     return []
+    #
+    # @classmethod
+    # def getToolDescription(cls):
+    #     """
+    #     Specifies a help text in HTML that is displayed below the tool.
+    #
+    #     Optional method. Default return value if method is not defined: ''
+    #     """
+    #     return ''
+    #
+    # @classmethod
+    # def getToolIllustration(cls):
+    #     """
+    #     Specifies an id used by StaticFile.py to reference an illustration
+    #     file on disk. The id is a list of optional directory names followed
+    #     by a filename. The base directory is STATIC_PATH as defined by
+    #     Config.py. The full path is created from the base directory
+    #     followed by the id.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def getFullExampleURL(cls):
+    #     """
+    #     Specifies an URL to an example page that describes the tool, for
+    #     instance a Galaxy page.
+    #
+    #     Optional method. Default return value if method is not defined: None
+    #     """
+    #     return None
+    #
+    # @classmethod
+    # def isDebugMode(cls):
+    #     """
+    #     Specifies whether the debug mode is turned on. Debug mode is
+    #     currently mostly used within the Genomic HyperBrowser and will make
+    #     little difference in a plain Galaxy ProTo installation.
+    #
+    #     Optional method. Default return value if method is not defined: False
+    #     """
+    #     return False
+    #
+    @classmethod
+    def getOutputFormat(cls, choices):
+        return 'gsuite'
+    #
+    # @classmethod
+    # def getOutputName(cls, choices=None):
+    #     return cls.getToolSelectionName()
+    #     """
+    #     The title (name) of the main output history element.
+    #
+    #     Optional method. Default return value if method is not defined:
+    #     the name of the tool.
+    #     """
