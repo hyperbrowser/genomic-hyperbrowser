e4455d83f39a0500152d4b5ae4b17a3db7c27339
dianadom <dianadom@ifi.uio.no>
2018-02-14 19:00:42 +0100

    hierarchical gSuite tool for mutation project

--- lib/hb/gold/gsuite/GSuiteEditor.py
+++ lib/hb/gold/gsuite/GSuiteEditor.py
@@ -51,3 +51,17 @@ def addColumnToGSuite(gsuite, uri, attrName, gSuiteTitle):
         outGSuite.addTrack(gsTrack)
     composeToFile(outGSuite, uri)
     return outGSuite
+
+def concatenateGSuitesAddingCategories(gSuiteList, categoryColumnTitle, categoryList):
+    concatenatedGSuite = GSuite()
+
+    assert len(gSuiteList) == len(categoryList)
+    for i, gSuite in enumerate(gSuiteList):
+        tracksToAdd = [track for track in gSuite.allTracks()]
+
+        for track in tracksToAdd:
+            track.setAttribute(categoryColumnTitle, categoryList[i])
+
+        concatenatedGSuite.addTracks(tracksToAdd)
+
+    return concatenatedGSuite
