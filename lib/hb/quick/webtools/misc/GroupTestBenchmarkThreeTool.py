a1353a879c8903a294f6ec934bb62dce1bbe6596
Boris Simovski <boris.simovski@gmail.com>
2018-04-03 14:14:35 +0200

    Implemented

---
+++ lib/hb/quick/webtools/misc/GroupTestBenchmarkThreeTool.py
@@ -0,0 +1,16 @@
+from quick.webtools.misc.GroupTestBenchmarkTwoTool import GroupTestBenchmarkTwoTool
+
+
+class GroupTestBenchmarkThreeTool(GroupTestBenchmarkTwoTool):
+    @classmethod
+    def getToolName(cls):
+        return "Group difference test - Benchmark three"
+
+
+    @staticmethod
+    def getOptionsBoxRandType(prevChoices):
+        return '__hidden__', 'Between tracks'
+
+    @staticmethod
+    def getOptionsBoxRandAlg(prevChoices):
+        return '__hidden__', 'Shuffle whole tracks (for categorical gsuites)'
