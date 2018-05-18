5dae8596ea934bbe5fdd8044c6b8c49daeaeba7f
Sveinung Gundersen <sveinugu@gmail.com>
2018-05-18 15:42:53 +0200

    Fixed MCFDR selection message inconsistency

--- lib/hb/gold/description/AnalysisList.py
+++ lib/hb/gold/description/AnalysisList.py
@@ -150,7 +150,7 @@ REPLACE_TEMPLATES['$MCFDRv4$']=\
 
 REPLACE_TEMPLATES['$MCFDRv5$']=\
 '''
- [setup:_MCFDR sampling depth=robust:Fixed 10 000 samples (slow)/high:Fixed 1 000 samples/medium:Fixed 100 samples (less stable indication)/scratch:Fixed 3 samples (unstable indication used for testing)]
+ [setup:_MCFDR sampling depth=robust:Fixed 10 000 samples (slow)/high:Fixed 1 000 samples/medium:Fixed 100 samples (less stable indication)/scratch:Fixed 2 samples (unstable indication used for testing)]
 
  [mcSamplerClass:<setup--scratch>=NaiveMCSampler:]
  [numSamplesPerChunk:<setup--scratch>=2:]
