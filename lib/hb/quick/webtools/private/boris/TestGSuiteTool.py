0ac45ed9fd1e823c83b3970cd77fd9315463dc22
Sveinung Gundersen <sveinugu@gmail.com>
2018-05-18 19:11:56 +0200

    Refactored the rest of the MCFDRv5 randomization tools to use RandAlgorithmMixin. Tools and analyses using older MCFDR setups is yet to be refactored.

--- lib/hb/quick/webtools/private/boris/TestGSuiteTool.py
+++ lib/hb/quick/webtools/private/boris/TestGSuiteTool.py
@@ -3,12 +3,13 @@ from gold.application.HBAPI import doAnalysis
 from gold.description.AnalysisDefHandler import AnalysisDefHandler
 from gold.description.AnalysisList import REPLACE_TEMPLATES
 from gold.track.TrackStructure import TrackStructureV2
+from gold.track.trackstructure.TsUtils import getRandomizedVersionOfTs
 from gold.track.trackstructure.random.ExcludedSegmentsStorage import ExcludedSegmentsStorage
 from gold.track.trackstructure.random.ShuffleElementsBetweenTracksAndBinsTvProvider import ShuffleElementsBetweenTracksAndBinsTvProvider
 from gold.track.trackstructure.random.ShuffleElementsBetweenTracksAndBinsTvProvider import \
     ShuffleElementsBetweenTracksAndBinsTvProvider
 from gold.track.trackstructure.random.TrackDataStorageRandAlgorithm import \
-    ShuffleElementsBetweenTracksAndBinsRandAlgorithm
+    CollisionDetectionTracksAndBinsRandAlgorithm
 from quick.application.ExternalTrackManager import ExternalTrackManager
 from quick.application.GalaxyInterface import GalaxyInterface
 from quick.multitrack.MultiTrackCommon import getGSuiteFromGalaxyTN
@@ -18,10 +19,11 @@ from quick.util.debug import DebugUtil
 from quick.webtools.GeneralGuiTool import GeneralGuiTool
 from quick.webtools.mixin.DebugMixin import DebugMixin
 from quick.webtools.mixin.GenomeMixin import GenomeMixin
+from quick.webtools.mixin.RandAlgorithmMixin import RandAlgorithmMixin
 from quick.webtools.mixin.UserBinMixin import UserBinMixin
 
 
-class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
+class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, RandAlgorithmMixin, DebugMixin):
     @classmethod
     def getToolName(cls):
         """
@@ -38,8 +40,8 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
             ('Select GSuite', 'gsuite'),
             ('Avoidance track', 'queryTrack')] + \
             cls.getInputBoxNamesForGenomeSelection() + \
-            [('Select excluded regions track', 'excludedRegions'),
-            ('Select MCFDR sampling depth', 'mcfdrDepth')] + \
+            cls.getInputBoxNamesForRandAlgSelection() + \
+            [('Select MCFDR sampling depth', 'mcfdrDepth')] + \
             cls.getInputBoxNamesForUserBinSelection() + \
             cls.getInputBoxNamesForDebug()
 
@@ -52,13 +54,8 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
         return GeneralGuiTool.getHistorySelectionElement()
 
     @classmethod
-    def getOptionsBoxExcludedRegions(cls, prevChoices):
-        return GeneralGuiTool.getHistorySelectionElement()
-
-    @classmethod
     def getOptionsBoxMcfdrDepth(cls, prevChoices):
         return AnalysisDefHandler(REPLACE_TEMPLATES['$MCFDRv5$']).getOptionsAsText().values()[0]
-
 
     @classmethod
     def execute(cls, choices, galaxyFn=None, username=''):
@@ -74,29 +71,21 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
         #                                                                              printProgress=False)
         # gsuite = getGSuiteFromGalaxyTN(choices.gsuite)
 
-        regSpec, binSpec = UserBinMixin.getRegsAndBinsSpec(choices)
-        analysisBins = GalaxyInterface._getUserBinSource(regSpec, binSpec, genome=genome)
+        analysisBins = UserBinMixin.getUserBinSource(choices)
 
         import quick.gsuite.GuiBasedTsFactory as factory
         queryTS = factory.getSingleTrackTS(genome, choices_queryTrack)
         refTS = factory.getFlatTracksTS(genome, choices_gsuite)
-
-        excludedTs = None
-        if choices.excludedRegions:
-            excludedTs = factory.getSingleTrackTS(genome, choices.excludedRegions)
 
         ts = TrackStructureV2()
         realTS = TrackStructureV2()
         realTS["query"] = queryTS
         realTS["reference"] = refTS
         randQueryTS = queryTS
-        excludedSegments = ExcludedSegmentsStorage(excludedTS=excludedTs, binSource=analysisBins)
-        randAlg = ShuffleElementsBetweenTracksAndBinsRandAlgorithm(
-            False, excludedSegmentsStorage=excludedSegments)
-        randTvProvider = ShuffleElementsBetweenTracksAndBinsTvProvider(randAlgorithm=randAlg,
-                                                                       origTs=refTS,
-                                                                       binSource=analysisBins)
-        randRefTS = refTS._getRandomizedVersion(randTvProvider, 0)
+        randTvProvider = cls.createTrackViewProvider(choices, refTS, analysisBins, genome)
+        localAnalysis = randTvProvider.supportsLocalAnalysis()
+        randRefTS = getRandomizedVersionOfTs(refTS, randTvProvider)
+
         hypothesisKeyList = [sts.metadata["title"] for sts in randRefTS.values()]
         for hypothesisKey in hypothesisKeyList:
             realTS = TrackStructureV2()
@@ -109,7 +98,7 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
             hypothesisTS["real"] = realTS
             hypothesisTS["rand"] = randTS
             ts[hypothesisKey] = hypothesisTS
-        analysisSpec = cls._prepareAnalysisWithHypothesisTests(choices)
+        analysisSpec = cls._prepareAnalysisWithHypothesisTests(choices, localAnalysis)
         if DebugConfig.USE_PROFILING:
             from gold.util.Profiler import Profiler
             profiler = Profiler()
@@ -121,9 +110,9 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
             if DebugConfig.USE_CALLGRAPH and galaxyFn:
                 profiler.printLinkToCallGraph(['profile_AnalysisDefJob'], galaxyFn)
         else:
-            result = doAnalysis(analysisSpec, analysisBins, ts).getGlobalResult()['Result']
-        print "Result", result
-
+            result = doAnalysis(analysisSpec, analysisBins, ts).getGlobalResult()["Result"]
+        for trackTitle, res in result.iteritems():
+            print '{}: {}<br>'.format(trackTitle, repr(res.getResult()))
 
         # tvProvider = ShuffleElementsBetweenTracksAndBinsTvProvider(refTS, queryTS, binSource=analysisBins, allowOverlaps=False)
         # for region in analysisBins:
@@ -132,7 +121,7 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
         # tvProvider._populatePool()
 
     @classmethod
-    def _prepareAnalysisWithHypothesisTests(cls, choices):
+    def _prepareAnalysisWithHypothesisTests(cls, choices, localAnalysis):
         mcfdrDepth = choices.mcfdrDepth if choices.mcfdrDepth else \
             AnalysisDefHandler(REPLACE_TEMPLATES['$MCFDRv5$']).getOptionsAsText().values()[0][0]
         analysisDefString = REPLACE_TEMPLATES['$MCFDRv5$'] + ' -> ' + ' -> MultipleRandomizationManagerStat'
@@ -142,7 +131,7 @@ class TestGSuiteTool(GeneralGuiTool, UserBinMixin, GenomeMixin, DebugMixin):
         analysisSpec.addParameter('pairedTsRawStatistic', ObservedVsExpectedStat.__name__)
         analysisSpec.addParameter('tail', 'right-tail')
         analysisSpec.addParameter('evaluatorFunc', 'evaluatePvalueAndNullDistribution')
-        analysisSpec.addParameter('tvProviderClass', ShuffleElementsBetweenTracksAndBinsTvProvider)
+        analysisSpec.addParameter('runLocalAnalysis', 'Yes' if localAnalysis else 'No')
         return analysisSpec
 
     @classmethod
