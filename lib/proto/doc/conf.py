7076d8827f46bbc44c452be549ce382b7a92e7c5
Sveinung Gundersen <sveinugu@gmail.com>
2019-01-25 23:07:19 +0100

    Bumped version to 1.2.2. Rebuilt documentation

--- lib/proto/doc/conf.py
+++ lib/proto/doc/conf.py
@@ -20,13 +20,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = u'Galaxy ProTo'
-copyright = u'2018, Morten Johansen, Sveinung Gundersen, Geir Kjetil Sandve'
+copyright = u'2019, Morten Johansen, Sveinung Gundersen, Geir Kjetil Sandve'
 author = u'Morten Johansen, Sveinung Gundersen, Geir Kjetil Sandve'
 
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.2.1'
+release = u'1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
