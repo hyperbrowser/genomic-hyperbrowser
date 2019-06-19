625f31aa4798928150e20ae6e096ad54e332db35
Sveinung Gundersen <sveinugu@gmail.com>
2019-06-19 01:20:23 +0100

    Some performance improvements in BigBedGenomeElementSource, especially for old analyze tool

--- lib/hb/gold/origdata/BigBedGenomeElementSource.py
+++ lib/hb/gold/origdata/BigBedGenomeElementSource.py
@@ -30,7 +30,7 @@ class BigBedGenomeElementSource(GenomeElementSource):
     def __init__(self, *args, **kwArgs):
         GenomeElementSource.__init__(self, *args, **kwArgs)
         self._bigBedFile = pyBigWig.open(self._fn)
-        self._chrIter = iter(sorted(self._bigBedFile.chroms().items()))
+        # self._chrIter = iter(sorted(self._bigBedFile.chroms().items()))
 
         self._extraColNames = self._initColumnNames()
         self._numOfExtraCols = 0
@@ -92,6 +92,23 @@ class BigBedGenomeElementSource(GenomeElementSource):
 
         #print ge
 
+        return ge
+
+    def parseFirstDataLine(self):
+        geIter = self.__iter__()
+        geIter._printWarnings = False
+
+        from gold.application.Logetup import logMessage
+        logMessage(currentChrom)
+
+        currentChrom = next(self._chrIter, None)
+        if not currentChrom:
+            self._bigBedFile.close()
+            raise InvalidFormatError('No chromosomes defined in BigBed file')
+
+        ge = GenomeElement(genome=self._genome, chr=currentChrom, start=np.array([0]), end=np.array([1]))
+        for colName in self._extraColNames():
+            setattr(ge, colName, np.array(['.']))
         return ge
 
     def _initExtraCols(self, entries):
